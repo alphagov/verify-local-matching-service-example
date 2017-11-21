5cade19b55fce949db26e295426b08a6cea689e5
Keerthana Subramanian <keerthas@thoughtworks.com>
2017-11-21 10:52:06 +0000

    TT-1460 : Added tests to check error scenarios
    Authors: @SKeerthana

--- src/test/java/uk/gov/ida/verifylocalmatchingserviceexample/resources/MatchingServiceResourceTest.java
+++ src/test/java/uk/gov/ida/verifylocalmatchingserviceexample/resources/MatchingServiceResourceTest.java
@@ -1,7 +1,9 @@
 package uk.gov.ida.verifylocalmatchingserviceexample.resources;
 
+import io.dropwizard.jersey.errors.ErrorMessage;
 import io.dropwizard.jersey.jackson.JsonProcessingExceptionMapper;
 import io.dropwizard.jersey.validation.JerseyViolationExceptionMapper;
+import io.dropwizard.jersey.validation.ValidationErrorMessage;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -14,7 +16,10 @@ import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static uk.gov.ida.verifylocalmatchingserviceexample.builders.MatchingServiceRequestDtoBuilder.aMatchingServiceRequestDtoBuilder;
 
 public class MatchingServiceResourceTest {
@@ -26,16 +31,71 @@ public class MatchingServiceResourceTest {
             .build();
 
     @Test
-    public void shouldReturnOKResponse() {
+    public void shouldReturnOKWhenThereIsNoConstraintViolation() {
         MatchingServiceRequestDto matchingServiceRequest = aMatchingServiceRequestDtoBuilder().build();
 
-        Response response = resources.target("/match-user")
-                .request()
-                .post(Entity.entity(matchingServiceRequest, MediaType.APPLICATION_JSON_TYPE));
+        Response response = postToMatchingService(matchingServiceRequest);
 
         assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-        HashMap<String, String> matchResponse = response.readEntity(new GenericType<HashMap<String, String>>() {{ }});
-        assertEquals(matchResponse.keySet(), new HashSet<String>() {{ add("result"); }});
+    }
+
+    @Test
+    public void shouldContainResultForSuccessResponse() {
+        MatchingServiceRequestDto matchingServiceRequest = aMatchingServiceRequestDtoBuilder().build();
+
+        Response response = postToMatchingService(matchingServiceRequest);
+
+        HashMap<String, String> matchResponse = response.readEntity(new GenericType<HashMap<String, String>>() {{
+        }});
+        assertEquals(matchResponse.keySet(), new HashSet<String>() {{
+            add("result");
+        }});
         assertEquals(matchResponse.get("result"), "match");
+    }
+
+    @Test
+    public void shouldReturn422WhenThereIsRequestValidationError() {
+        MatchingServiceRequestDto matchingServiceRequest = aMatchingServiceRequestDtoBuilder()
+                .withMatchingAttributesDto(null)
+                .build();
+
+        Response response = postToMatchingService(matchingServiceRequest);
+
+        assertThat(response.getStatus()).isEqualTo(422);
+
+        ValidationErrorMessage errorMessage = response.readEntity(ValidationErrorMessage.class);
+        assertThat(errorMessage.getErrors().size()).isEqualTo(1);
+        assertThat(errorMessage.getErrors().get(0)).isEqualTo("matchingAttributesDto may not be null");
+    }
+
+    @Test
+    public void shouldReturn422ForBadJson() {
+        Response response = resources.target("/match-user")
+                .request()
+                .post(Entity.entity("{}", MediaType.APPLICATION_JSON_TYPE));
+
+        assertThat(response.getStatus()).isEqualTo(422);
+
+        ValidationErrorMessage errorMessage = response.readEntity(ValidationErrorMessage.class);
+        assertFalse(errorMessage.getErrors().isEmpty());
+    }
+
+    @Test
+    public void shouldReturn400ForMalformedJson() {
+        Response response = resources.target("/match-user")
+                .request()
+                .post(Entity.entity("not valid json", MediaType.APPLICATION_JSON_TYPE));
+
+        assertThat(response.getStatus()).isEqualTo(BAD_REQUEST.getStatusCode());
+
+        ErrorMessage actualError = response.readEntity(ErrorMessage.class);
+        assertThat(actualError.getCode()).isEqualTo(BAD_REQUEST.getStatusCode());
+        assertThat(actualError.getMessage()).isEqualTo("Unable to process JSON");
+    }
+
+    private Response postToMatchingService(MatchingServiceRequestDto matchingServiceRequest) {
+        return resources.target("/match-user")
+                .request()
+                .post(Entity.entity(matchingServiceRequest, MediaType.APPLICATION_JSON_TYPE));
     }
 }
