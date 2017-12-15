4a05fb7398b256b97778ace7a01a53a978770b17
Hristo Georgiev <hristo.georgiev@digital.cabinet-office.gov.uk>
2017-12-15 13:54:25 +0000

    TT-1733: Introduces a healthcheck for DB connection
    Authors: @georgievh

--- src/main/java/uk/gov/ida/verifylocalmatchingserviceexample/VerifyLocalMatchingServiceExampleFactory.java
+++ src/main/java/uk/gov/ida/verifylocalmatchingserviceexample/VerifyLocalMatchingServiceExampleFactory.java
@@ -1,28 +1,33 @@
 package uk.gov.ida.verifylocalmatchingserviceexample;
 
+import com.codahale.metrics.health.HealthCheck;
 import org.flywaydb.core.Flyway;
 import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import uk.gov.ida.verifylocalmatchingserviceexample.configuration.DatabaseEngine;
 import uk.gov.ida.verifylocalmatchingserviceexample.dataaccess.PersonDAO;
 import uk.gov.ida.verifylocalmatchingserviceexample.dataaccess.VerifiedPidDAO;
 import uk.gov.ida.verifylocalmatchingserviceexample.db.migration.DatabaseMigrationRunner;
+import uk.gov.ida.verifylocalmatchingserviceexample.healtchecks.DatabaseHealthCheck;
 import uk.gov.ida.verifylocalmatchingserviceexample.resources.MatchingServiceResource;
 import uk.gov.ida.verifylocalmatchingserviceexample.service.Cycle0MatchingService;
 import uk.gov.ida.verifylocalmatchingserviceexample.service.Cycle1MatchingService;
 import uk.gov.ida.verifylocalmatchingserviceexample.service.MatchingService;
 
 public class VerifyLocalMatchingServiceExampleFactory {
+
+
     public DatabaseMigrationRunner getDatabaseMigrationRunner(DatabaseEngine databaseEngine) {
         return new DatabaseMigrationRunner(new Flyway(), databaseEngine);
     }
 
-    public MatchingServiceResource getMatchingService(String url) {
-        Jdbi jdbi = Jdbi.create(url);
-        jdbi.installPlugin(new SqlObjectPlugin());
-
+    public MatchingServiceResource getMatchingService(Jdbi jdbi) {
         Cycle0MatchingService cycle0MatchingService = new Cycle0MatchingService(new VerifiedPidDAO(jdbi));
         Cycle1MatchingService cycle1MatchingService = new Cycle1MatchingService(new PersonDAO(jdbi));
+
         return new MatchingServiceResource(new MatchingService(cycle0MatchingService, cycle1MatchingService));
+    }
+
+    public HealthCheck getDatabaseHealthCheck(PersonDAO personDAO) {
+        return new DatabaseHealthCheck(personDAO);
     }
 }
