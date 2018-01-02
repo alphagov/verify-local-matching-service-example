efb3cec41138a36bb940e3f0d8ac2064601c6dc0
Andy Paine <andy.paine@outlook.com>
2018-01-02 17:47:01 +0000

    TT-1775: Add png generation for schema diagram
    HTML doesn't render nicely in Github by default so we are adding
    a method to generate a PNG as well so that the schema diagram is
    easily available
    
    Authors: @andy-paine

--- create-db-schema-docs.sh
+++ create-db-schema-docs.sh
@@ -7,6 +7,7 @@ docker run -d --network=db_network --name schemacrawler -v $(pwd)/docs:/share --
 # There is an issue with graphviz output in HTML mode that means this line must be removed
 docker exec schemacrawler sed -i "s/schemacrawler.graph.graphviz_opts=-Gdpi=300//" config/schemacrawler.config.properties
 docker exec schemacrawler ./schemacrawler.sh -server=postgresql -host=postgres_for_matching_service -user=postgres -database=postgres -infolevel=standard -command=schema -outputformat=htmlx -o /share/schema.html -grepcolumns=".*schema_version.*" -invert-match -title="Example Local Matching Service Database Schema"
+docker exec schemacrawler ./schemacrawler.sh -server=postgresql -host=postgres_for_matching_service -user=postgres -database=postgres -infolevel=standard -command=schema -outputformat=png -o /share/schema.png -grepcolumns=".*schema_version.*" -invert-match -title="Example Local Matching Service Database Schema"
 
 docker kill schemacrawler
 ./shutdown.sh
